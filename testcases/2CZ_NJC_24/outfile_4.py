0b1fe2bf32f316d530a421d9d0a96b66e3cdd093
ffgtfgh <152868614+ffgtfgh53@users.noreply.github.com>
2026-04-02 10:25:04 +0800

    Add Testtask4_2

--- testcases/2CZ_NJC_24/outfile_4.py
+++ testcases/2CZ_NJC_24/outfile_4.py
@@ -1,3 +1,5 @@
+# type: ignore 
+# (disables linters such as pyright)
 def task4_1():
     #  Task 4.1
 
@@ -29,3 +31,48 @@ def task4_1():
         conn.execute(query)
     conn.commit()
     conn.close()
+
+# Task 4.2
+import string
+
+class Person:
+    def __init__(self,full_name,date_of_birth):
+        self.full_name = full_name
+        self.date_of_birth = date_of_birth
+    def is_player(self):
+        return "Maybe"
+    def is_staff(self):
+        return "Maybe"
+    def event_name(self):
+        name = self.full_name
+        dob = self.date_of_birth
+        stripped = [i for i in name if i not in (string.punctuation + " ")]
+        y,m,d = dob.split("-")
+        stripped += [m,d]
+        event_name = "".join(stripped)
+        return event_name
+    
+
+class Player(Person):
+    def __init__(self,full_name,date_of_birth,team_name,char_name,score):
+        super().__init__(full_name,date_of_birth)
+        self.char_name = char_name
+        self.team_name = team_name
+        self.score = score
+    def event_name(self):
+        char_name = self.char_name
+        team_name = self.team_name
+        event_name = f"{char_name} <{team_name}>"
+        return event_name
+    def is_player(self):
+        return True 
+    
+class Staff(Person):
+    def __init__(self,full_name,date_of_birth):
+        super().__init__(full_name,date_of_birth)
+    def is_staff(self):
+        return True
+    def event_name(self):
+        p_event_name = super().event_name()
+        event_name = p_event_name + "Staff"
+        return event_name
--- testcases/2CZ_NJC_24/test_2CZ_NJC_24_TASK4.py
+++ testcases/2CZ_NJC_24/test_2CZ_NJC_24_TASK4.py
@@ -1,14 +1,15 @@
-from enum import verify
 import re
 import sqlite3
 import unittest
-from unittest.mock import mock_open, patch
+from unittest.mock import patch
+from random import randint
+from itertools import cycle
 
 from python_testcase_functions import NoMoreClosingFunction
 
-from .outfile_4 import task4_1
+from .outfile_4 import task4_1, Person, Player, Staff
 
-class Testtask4_1(unittest.TestCase):
+class TestTask4_1(unittest.TestCase):
     def setUp(self):
         self.database_conn = sqlite3.connect(":memory:")
         with patch('sqlite3.connect', side_effect=self.mock_connect):
@@ -91,3 +92,71 @@ class Testtask4_1(unittest.TestCase):
             self.verify_column_type(player, expected_player_table)
         with self.subTest("Verify foreign keys in PLAYER"):
             self.verify_foreign_key(player_fk, expected_player_table)
+
+class TestTask4_2(unittest.TestCase):
+    def test_class_inheritance(self):
+        self.assertIn(Person, Player.mro(), "Player must inherit from Person")
+        self.assertIn(Person, Staff.mro(), "Staff must inherit from Person")
+
+    def test_class_person(self):
+        testcases = [
+            ({"full_name": "Bira Dawn", "date_of_birth": "1985-10-02"}, "BiraDawn1002"),
+            ({"full_name": "George Robinson Jr.", "date_of_birth": "7654-03-21"}, "GeorgeRobinsonJr0321"),
+            ({"full_name": "Harrison Ford", "date_of_birth": "1942-07-13"}, "HarrisonFord0713"),
+            ({"full_name": "Mr. Bean", "date_of_birth": "1990-01-01"}, "MrBean0101"),
+            ({"full_name": "...........", "date_of_birth": "0000-00-00"}, "0000"),
+            ({"full_name": "", "date_of_birth": "0000-11-22"}, "1122"),
+            ({"full_name": "X!@#n.o,?P;':y\"`-t", "date_of_birth": "0101-01-01"}, "XnoPyt0101")
+        ]
+        for kwargs, ans in testcases:
+            with self.subTest("Test Person with arguments", **kwargs):
+                try:
+                    p = Person(**kwargs)
+                except TypeError:
+                    self.fail("Person.__init__() should accept correct keyword argumnets")
+                self.assertEqual(p.is_player(), 'Maybe')
+                self.assertEqual(p.is_staff(), 'Maybe')
+                self.assertEqual(p.event_name(), ans)
+    
+    def test_class_player(self):
+        testcases = [
+            ({"full_name": "Bira Dawn", "date_of_birth": "1985-10-02", "char_name": "Rogerbrown", "team_name": "Echo"}, "Rogerbrown <Echo>"),
+            ({"full_name": "George Robinson Jr.", "date_of_birth": "7654-03-21", "char_name": "wow look!", "team_name": "hehe i do cp"}, "wow look! <hehe i do cp>"),
+            ({"full_name": "Harrison Ford", "date_of_birth": "1942-07-13", "char_name": "Ford does it best", "team_name": "Automobile supremacy"}, "Ford does it best <Automobile supremacy>"),
+            ({"full_name": "Mr. Bean", "date_of_birth": "1990-01-01", "char_name": "Mr. Bean", "team_name": "no1 jokers"}, "Mr. Bean <no1 jokers>"),
+            ({"full_name": "...........", "date_of_birth": "0000-00-00", "char_name": " ", "team_name": " "}, "  < >"),
+            ({"full_name": "", "date_of_birth": "0000-11-22", "char_name": '', "team_name": ''}, " <>"),
+            ({"full_name": "X!@#n.o,?P;':y\"`-t", "date_of_birth": "0101-01-01", "char_name": '123!@#', "team_name": "098)(*)"}, "123!@# <098)(*)>")
+        ]
+        for kwargs, ans in testcases:
+            score = randint(0, 5000)
+            with self.subTest("Test Player with arguments", score=score, **kwargs):
+                try:
+                    p = Player(**kwargs, score=score)
+                except ValueError:
+                    self.fail("Person.__init__() should accept correct keyword arguments")
+                self.assertIsInstance(p.is_player(), bool, "is_player() should return a boolean")
+                self.assertTrue(p.is_player(), "is_player() should return True")
+                self.assertEqual(p.event_name(), ans, "Incorrect return value of event_name()")
+    
+    def test_class_staff(self):
+        testcases = [
+            ({"full_name": "Bira Dawn", "date_of_birth": "1985-10-02"}, "BiraDawn1002"),
+            ({"full_name": "George Robinson Jr.", "date_of_birth": "7654-03-21"}, "GeorgeRobinsonJr0321"),
+            ({"full_name": "Harrison Ford", "date_of_birth": "1942-07-13"}, "HarrisonFord0713"),
+            ({"full_name": "Mr. Bean", "date_of_birth": "1990-01-01"}, "MrBean0101"),
+            ({"full_name": "...........", "date_of_birth": "0000-00-00"}, "0000"),
+            ({"full_name": "", "date_of_birth": "0000-11-22"}, "1122"),
+            ({"full_name": "X!@#n.o,?P;':y\"`-t", "date_of_birth": "0101-01-01"}, "XnoPyt0101")
+        ]
+        for kwargs, ans in testcases:
+            with self.subTest("Test staff with argumants", **kwargs):
+                try:
+                    s = Staff(**kwargs)
+                except ValueError:
+                    self.fail("Staff.__init__() should accept correct keyword arguments")
+                self.assertIsInstance(s.is_staff(), bool, "is_staff() should return a boolean")
+                self.assertTrue(s.is_staff(), "is_staff() should return True")
+                self.assertTrue(s.event_name().startswith(ans), "event_name() must start with Player.event_name")
+                self.assertTrue(s.event_name().endswith('Staff'), "event_name() must end with 'Staff'")
+                self.assertLessEqual(len(s.event_name()), len(ans)+6, "event_name() too long")
