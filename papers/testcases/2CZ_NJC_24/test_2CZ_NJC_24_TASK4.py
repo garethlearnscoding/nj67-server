203129c2d465de5fd02a673c3594726a772ac7d4
ffgtfgh <152868614+ffgtfgh53@users.noreply.github.com>
2026-04-02 10:51:49 +0800

    shift sqlite3 testing functions to python_testcase_functions

--- papers/testcases/2CZ_NJC_24/test_2CZ_NJC_24_TASK4.py
+++ papers/testcases/2CZ_NJC_24/test_2CZ_NJC_24_TASK4.py
@@ -3,9 +3,8 @@ import sqlite3
 import unittest
 from unittest.mock import patch
 from random import randint
-from itertools import cycle
 
-from python_testcase_functions import NoMoreClosingFunction
+from python_testcase_functions import NoMoreClosingFunction, sqlite3_verify_table
 
 from .outfile_4 import task4_1, Person, Player, Staff
 
@@ -20,31 +19,6 @@ class TestTask4_1(unittest.TestCase):
             return NoMoreClosingFunction(self.database_conn)
         else:
             self.fail(f"Wrong filename {database}")
-
-    def verify_column_type(self, table: sqlite3.Cursor, expected: dict[str, dict[str, dict]]):
-        no_row = 0
-        for _, col_name, col_type, _, _, col_pk in table:
-            try:
-                self.assertEqual(expected['cols'][col_name]['type'], col_type.upper())
-                if expected['cols'][col_name].get('pk', False):
-                    self.assertTrue(col_pk, f"{col_name} is not primary key")
-                no_row += 1
-            except KeyError:
-                self.fail(f"Unknown or extra column {col_name}")
-        self.assertEqual(len(expected['cols']), no_row, "Missing column(s)")
-
-    def verify_foreign_key(self, table: sqlite3.Cursor, expected: dict[str, dict[str, dict]]):
-        no_fk = 0
-        for _, _, org_table_name, org_name, curr_name, _, _, _ in table:
-            try:
-                self.assertEqual((org_table_name, org_name),
-                    (expected['fk'][curr_name]['ref_table'], expected['fk'][curr_name]['ref_col']),
-                    "Foreign key mismatch")
-                no_fk += 1
-            except KeyError:
-                self.fail(f"Unknown or extra foreign key {curr_name}")
-        self.assertEqual(len(expected['fk']), no_fk, "Missing foreign key(s)")
-
         
     def test_tables_exists(self):
         "Test task 4.1 has neccessary tables"
@@ -53,8 +27,6 @@ class TestTask4_1(unittest.TestCase):
         self.assertIn(("PEOPLE",), tables, "People table missing from database")
     
     def test_table_people(self):
-        people = self.database_conn.execute("PRAGMA table_info(PEOPLE)")
-        people_fk = self.database_conn.execute("PRAGMA foreign_key_list(PEOPLE)")
         expected_people_table = {
             'cols': {
                 'PersonID': { 'type': 'INTEGER', 'pk': True},
@@ -65,14 +37,9 @@ class TestTask4_1(unittest.TestCase):
             },
             'fk': {}
         }
-        with self.subTest("Verify types of each column in PEOPLE"):
-            self.verify_column_type(people, expected_people_table)
-        with self.subTest("Verify foreign keys in PEOPLE"):
-            self.verify_foreign_key(people_fk, expected_people_table)        
+        sqlite3_verify_table(self, self.database_conn, "PEOPLE", expected_people_table)
 
     def test_table_player(self):
-        player = self.database_conn.execute("PRAGMA table_info(PLAYER)")
-        player_fk = self.database_conn.execute("PRAGMA foreign_key_list(PLAYER)")
         expected_player_table = {
             'cols': {
                 'PersonID': { 'type': 'INTEGER', 'pk': True },
@@ -88,10 +55,7 @@ class TestTask4_1(unittest.TestCase):
                 }
             }
         }
-        with self.subTest("Verify types of each column in PLAYER"):
-            self.verify_column_type(player, expected_player_table)
-        with self.subTest("Verify foreign keys in PLAYER"):
-            self.verify_foreign_key(player_fk, expected_player_table)
+        sqlite3_verify_table(self, self.database_conn, "PLAYER", expected_player_table)
 
 class TestTask4_2(unittest.TestCase):
     def test_class_inheritance(self):
